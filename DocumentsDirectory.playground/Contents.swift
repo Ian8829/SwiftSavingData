a7eea7f8f2bdee7cc4c803cdaa07fc91dbc774f7
ghjkghj <ghjkghj@naver.com>
2021-07-22 14:13:08 +0900

    path and url

--- DocumentsDirectory.playground/Contents.swift
+++ DocumentsDirectory.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Foundation
 
-FileManager.documentDirectoryURL
+let remindersDataURL = URL(
+	fileURLWithPath: "Reminders",
+	relativeTo: FileManager.documentDirectoryURL
+)
+
+let stringURL = FileManager.documentDirectoryURL
+	.appendingPathComponent("String")
+	.appendingPathExtension("txt")
+
+stringURL.path
+
+let challeangeString: String = "To Do List"
+let challeangeURL: URL = URL(
+	fileURLWithPath: challeangeString,
+	relativeTo:	FileManager.documentDirectoryURL
+).appendingPathExtension("txt")
+
+print(challeangeURL.lastPathComponent)
